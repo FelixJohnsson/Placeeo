ff714fa1b4d40b7e2ed9173f500e34523c8f8f82
FelixJohnsson <felix_.johnson@hotmail.com>
2020-10-04 00:00:23 +0200

    Text file handler

--- server.js
+++ server.js
@@ -29,31 +29,69 @@ app.post('/imageData', function(req, res) {
     imageData.id = uuidv4();
     username = req.body.user;
     imageData.date = imageData.date.replace(':', '-')
-    console.log(imageData)
     handleFolders(req.body.user);
 });
 
 let newImageName;
 app.post('/image-upload', (req, res) => {
     newImageName = req.files.file.name;
-    req.files.file.mv(`${__dirname}\\static\\images\\${username}\\${imageData.date}-${imageData.time}-${imageData.location[0]}-${imageData.location[1]}.png`)
+    let newFileName = `${__dirname}\\static\\images\\${username}\\${imageData.date}-${imageData.time}-${imageData.location[0]}-${imageData.location[1]}.png`
+    req.files.file.mv(newFileName)
+    newFileData = {
+      username: username, 
+      date: `${imageData.date}-${imageData.time}`,
+      location: `${imageData.location[0]}-${imageData.location[1]}`
+    }
+    writeFile(`${__dirname}\\static\\images\\${username}\\${username}.txt`, JSON.stringify(fileData))
+    let txtFile = readTxtFile(`${__dirname}\\static\\images\\${username}\\${username}.txt`);
+    console.log(txtFile)
     res.redirect('/home');
 });
 
 function handleFolders(username){
+  let newUser = null;
   fs.mkdir(`./static/images/${username}`, function(err) {
     if (err) {
-      console.log(err.code)
+      newUser = true;
     } else {
-      console.log("New directory successfully created.")
+      newUser = false;
     }
   })
+  console.log(newUser)
+  return newUser;
+}
+let fileData = 'No data';
+function readTxtFile(username){
+  let users = fs.readdirSync(`${__dirname}\\static\\images\\`)
+  users.forEach(el => {
+    if (el === username){
+      fs.readFile(fileDir, (err, data) => { 
+        if (err) throw err; 
+       fileData = JSON.parse(data) 
+    }) 
+    }
+  })
+  return fileData;
+}
+function writeFile(fileDir, data){
+  fs.writeFile(fileDir, data, function (err) {
+    if (err) throw err;
+    return true;
+  });
 }
 
 app.get('/images/:username/', (req, res) => {
-  const files = fs.readdirSync(`${__dirname}\\static\\images\\${req.params.username}\\`);
-  res.send(files)
+  
+  const files = fs.readdirSync(`${__dirname}\\static\\images\\`)
+  files.forEach(el => {
+    if (el === req.params.username){
+      res.send(fs.readdirSync(`${__dirname}\\static\\images\\${req.params.username}`))
+    }
+  })
+  
 })
 app.get('/images/:username/:id', (req, res) => {
   res.sendFile(`${__dirname}\\static\\images\\${req.params.username}\\${req.params.id}`)
 })
+
+
